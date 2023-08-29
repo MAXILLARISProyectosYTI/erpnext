29eac6c5cab1430b037a1c6c067b9aced84d8cab
MAXILLARISProyectosYTI <135283519+MAXILLARISProyectosYTI@users.noreply.github.com>
2023-08-29 15:13:28 -0500

    Update __init__.py

--- erpnext/__init__.py
+++ erpnext/__init__.py
@@ -2,25 +2,11 @@ import functools
 import inspect
 
 import frappe
-
 __version__ = "15.0.0-dev"
 
 
 def get_default_company(user=None):
 	"""Get default company for user"""
-	from frappe.defaults import get_user_default_as_list
-
-	if not user:
-		user = frappe.session.user
-
-	companies = get_user_default_as_list(user, "company")
-	if companies:
-		default_company = companies[0]
-	else:
-		default_company = frappe.db.get_single_value("Global Defaults", "default_company")
-
-	return default_company
-
 
 def get_default_currency():
 	"""Returns the currency of the default company"""
